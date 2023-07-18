ea2e0ca5ef344abd6baf5aebef9d9420555ce7f9
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2023-07-18 19:02:39 -0400

    adding build file

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('post-data', function(req) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'));
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 
 let contacts = [
